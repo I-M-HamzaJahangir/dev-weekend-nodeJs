5b902bfac08c0e9f88b77c0b567569b90a8cb546
Muhammad Hamza Jahangeer <muhammad.hamza03@pgc.edu.pk>
2026-06-29 11:46:19 +0500

    setup frontend and backend for blogging-app

---
+++ blogging-app-fe/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1>
+          This is the front end side of my blogging app i am currently working
+          on
+        </h1>
+        <h2>Coming Soon...</h2>
+      </section>
+    </>
+  );
+}
+
+export default App;
